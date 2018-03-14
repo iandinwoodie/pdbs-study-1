752ce31a9aceee1eb106a9c70539a2366620c012
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-03-13 22:30:08 -0400

    Added duplicate checks for shared list of dogs

--- pdbs/phase_1/scripts/scrub_duplicates.py
+++ pdbs/phase_1/scripts/scrub_duplicates.py
@@ -43,6 +43,7 @@ if infile == outfile:
     outfile = outfile + '.temp'
 
 # Parse the raw data with relevant filters.
+status_sum_dict = {}
 dupe_cnt = 0
 status_dict = {}
 data_dict = {}
@@ -58,36 +59,39 @@ with open(infile, 'r') as fin:
                 status = [int(row[10]), int(row[145]), int(row[280]),
                           int(row[415]), int(row[550]), int(row[688])
                 ]
+                status_sum = sum(status)
                 dogs = [row[11], row[146], row[281], row[416], row[551]]
                 dogs = [x for x in dogs if x != '']
                 if user_hash in status_dict:
                     dupe_cnt += 1
                     if status[5] == 0:
-                        # Skip incomplete duplicate.
-                        continue
-                    if status_dict[user_hash][5] == 0:
-                        # Replace incomplete entries with complete entries.
+                        print('Duplicate found: updated entry is incomplete, '
+                              'discard updated')
+                    elif status_dict[user_hash][5] == 0:
+                        print('Duplicate found: existing entry is incomplete, '
+                              'save updated')
                         write_row = True
-                        continue
-                    old_dogs = set(dog_dict[user_hash])
-                    new_dogs = set(dogs)
-                    if not bool(old_dogs.symmetric_difference(new_dogs)):
-                        # For matching dog entries, assume the user wanted to
-                        # change their existing response.
-                        write_row = True
-                        continue
                     else:
-                        # For mismatched dog entries, see if there are similarities.
-                        print('Error: multiple entries for %s' %user_hash)
-                        print(dog_dict[user_hash], end=' - ')
-                        print(status_dict[user_hash])
-                        print(dogs, end=' - ')
-                        print(status)
-                    #print(bool(set(dogs).symmetric_difference(dog_dict[user_hash])))
-                    #if dog_dict[user_hash] == dogs and status_dict[user_hash] == status:
-                        #print('MATCH %s %s' %(data_dict[user_hash][0], row[0]))
-                        #print(set(data_dict[user_hash][1:]).symmetric_difference(row[1:]))
-
+                        old_dogs = set(dog_dict[user_hash])
+                        new_dogs = set(dogs)
+                        if not bool(old_dogs.symmetric_difference(new_dogs)):
+                            print('Duplicate found: same set of dogs, ', end='')
+                            if status_sum_dict[user_hash] <= status_sum:
+                                print('but more complete data, save updated')
+                                write_row = True
+                            else:
+                                print('but less complete data, discard updated')
+                        else:
+                            # For mismatched dog entries, see if there are similarities.
+                            print('Duplicate found: UNCLASSIFIED')
+                        #print(bool(set(dogs).symmetric_difference(dog_dict[user_hash])))
+                        #if dog_dict[user_hash] == dogs and status_dict[user_hash] == status:
+                            #print('MATCH %s %s' %(data_dict[user_hash][0], row[0]))
+                            #print(set(data_dict[user_hash][1:]).symmetric_difference(row[1:]))
+                    print(dog_dict[user_hash], end=' - ')
+                    print(status_dict[user_hash])
+                    print(dogs, end=' - ')
+                    print(status)
                 else:
                     write_row = True
             else:
@@ -96,6 +100,7 @@ with open(infile, 'r') as fin:
                 data_dict[user_hash] = row
                 status_dict[user_hash] = status
                 dog_dict[user_hash] = dogs
+                status_sum_dict[user_hash] = status_sum
                 #writer.writerow(row)
 
 # If a temp file was used, move the results back to the requested destination.

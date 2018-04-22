c91bc47a09f7ce6fb99a634c9081750e9eb2b0ae
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-04-21 23:38:14 -0400

    Updating raw data fetch script

--- src/data/fetch_raw_data.py
+++ src/data/fetch_raw_data.py
@@ -2,25 +2,24 @@ from dotenv import find_dotenv, load_dotenv
 from requests import post
 import logging
 import os
+import json
 
 
 def main():
     database_url = os.environ.get("DATABASE_URL")
     api_token = os.environ.get("API_TOKEN")
-    print(api_token)
-
     payload = {
         'token': api_token,
         'format': 'json',
         'content': 'record'
         }
-
-    response = post(database_url, data=payload)
-    print (response.status_code)
-
-    data = response.json()
     logger = logging.getLogger(__name__)
     logger.info('fetching raw data from database')
+    response = post(database_url, data=payload)
+    logger.info('saving data to file')
+    data = response.json()
+    with open('raw.json', 'w') as fout:
+        json.dump(data, fout)
 
 
 if __name__ == "__main__":

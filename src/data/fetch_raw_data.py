fd2eea2a370e826414af6020835687f5211171c9
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-04-22 18:31:39 -0400

    Store data as csv instead of json to conserve disk space

--- src/data/fetch_raw_data.py
+++ src/data/fetch_raw_data.py
@@ -1,25 +1,31 @@
 from dotenv import find_dotenv, load_dotenv
 from requests import post
+import csv
 import logging
 import os
-import json
 
 
 def main():
+    """Fetching the raw data from REDCap."""
+    logger = logging.getLogger(__name__)
+
+    logger.info('fetching raw data from database')
     database_url = os.environ.get("DATABASE_URL")
     api_token = os.environ.get("API_TOKEN")
     payload = {
         'token': api_token,
-        'format': 'json',
+        'format': 'csv',
         'content': 'record'
         }
-    logger = logging.getLogger(__name__)
-    logger.info('fetching raw data from database')
     response = post(database_url, data=payload)
+    data = response.text.splitlines()
+
     logger.info('saving data to file')
-    data = response.json()
-    with open('raw.json', 'w') as fout:
-        json.dump(data, fout)
+    with open('raw.csv', 'w') as fout:
+        writer = csv.writer(fout)
+        reader = csv.reader(data)
+        for row in reader:
+            writer.writerow(row)
 
 
 if __name__ == "__main__":
